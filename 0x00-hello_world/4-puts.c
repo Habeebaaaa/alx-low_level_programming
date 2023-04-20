847d1b8b9bae3114f392c720d5e8376ace6f86af
habeebaaaa <habebaghanem9999@gmail.com>
2023-04-20 14:24:39 -0700

    combiler

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Dwscroption: A C programm that prints with put function
+ *
+ * Return: Always 0 (SSuccess)
+*/
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
