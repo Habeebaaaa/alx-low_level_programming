d0bbf48193110d24865fe27d8787c2c1a14f3275
habeebaaaa <habebaghanem9999@gmail.com>
2023-04-20 15:50:18 -0700

    compiler

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,16 @@
 */
 int main(void)
 {
-printf("size of a char : %lu byte(s) \n", sizeof(cahr));
-printf("size of an int : %lu byte(s) \n", sizeof(int));
-printf("size of a long int : %lu byte(s) \n", sizeof(long int));
-printf("size of a long long int : %lu byte(s) \n", sizeof(long long int));
-printf("size of a float : %lu byte(s) \n", sizeof(float)):
+	char a;
+	int b;
+	long int c;
+	long long int;
+	float f;
+
+printf("size of a char: %lu byte(s) \n", sizeof(cahr));
+printf("size of an int: %lu byte(s) \n", sizeof(int));
+printf("size of a long int: %lu byte(s) \n", sizeof(long int));
+printf("size of a long long int: %lu byte(s) \n", sizeof(long long int));
+printf("size of a float: %lu byte(s) \n", sizeof(float)):
 return (0);
 }
