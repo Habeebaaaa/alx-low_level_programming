2d508925bfafd7bb74964d076a3ee2770d0cec18
habeebaaaa <habebaghanem9999@gmail.com>
2023-04-23 14:08:07 -0700

    a

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * description: false or true validation
+ *
+ * Return: 0 (seccess)
+*/
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	return (0);
+}
+
