718c51f0de99d8405e95665f4b62e82b559c4696
habeebaaaa <habebaghanem9999@gmail.com>
2023-04-24 07:38:30 -0700

    a

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+*main - entry point
+*
+*description: print alphabet in reverse
+*
+*Return: 0 (seccess)
+*/
+
+int main(void)
+{
+	char ch = 'z';
+
+	while (ch <= 'a')
+{
+	putchar(ch);
+	ch--;
+}
+putchar('\n');
+
+return (0);
+}
