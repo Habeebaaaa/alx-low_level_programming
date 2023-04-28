732d221a86c86bdfa2ee8b3a8d295cc5dd9497f7
habeebaaaa <habebaghanem9999@gmail.com>
2023-04-27 18:26:41 -0700

    a

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,14 +11,16 @@ void print_diagonal(int n)
 	int postn, space;
 
 	if (n <= 0)
+	{
 		_putchar('\n');
+	}
 	else
 	{
 		for (postn = 0; postn <= n; postn++)
 		{
 			for (space = 0; space <= postn; space++)
 				_putchar(' ');
-			_putchar(92); /*is equal '/' char*/
+			_putchar(92);
 			_putchar('\n');
 		}
 	}
