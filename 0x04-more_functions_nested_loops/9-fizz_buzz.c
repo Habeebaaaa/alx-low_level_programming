43de835395d087189488d1e410d91db0e2053930
habeebaaaa <habebaghanem9999@gmail.com>
2023-04-27 19:12:03 -0700

    a

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,18 +13,28 @@ int main(void)
 
 	for (num - 1; num <= 100; num++)
 	{
-		if (num % 3 == 0 && !(num % 5 == 0))
+		if (num % 3 == 0)
+		{
 			printf("Fizz");
-		else if (num % 5 == 0 & !(num % 3 == 0))
+		}
+		else if (num % 5 == 0)
+		{
 			printf("Buzz");
+		}
 		else if (num % 3 == 0 && num % 5 == 0)
+		{
 			printf("FizzBuzz");
+		}
 		else
+		{
 			printf("%d", num);
+		}
 		if (num != 100)
+		{
 			printf(" ");
-		else
-			printf("\n");
+		}
 	}
+	printf("\n");
+
 	return (0);
 }
