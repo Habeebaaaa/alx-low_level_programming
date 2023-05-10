749445cd20bcbf845595030d0277e5d60be4c92b
habeebaaaa <habebaghanem9999@gmail.com>
2023-05-09 17:59:10 -0700

    a

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strpbrk - a function that searches a string for any of a set of bytes.
+ *
+ * @s: string
+ * @accept: input
+ *
+ * Return: always 0
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int x, y;
+	char *p;
+
+	x = 0;
+	while (s[x] != '\0')
+	{
+		y = 0;
+		while (accept[y] != '\0')
+		{
+			if (accept[y] == s[x])
+			{
+				p = &s[x];
+				return (p);
+			}
+			y++;
+		}
+		x++;
+	}
+	return (0);
+}
---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strstr - a function that locates a substring.
+ *
+ * @haystack: input
+ * @needle: input
+ *
+ * Return: always 0
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	for (;
