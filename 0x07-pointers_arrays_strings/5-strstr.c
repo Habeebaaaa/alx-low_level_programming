2ef1db807f86f20776aeeb9395337b76765368a3
habeebaaaa <habebaghanem9999@gmail.com>
2023-05-09 18:30:44 -0700

    a

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,4 +11,18 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-	for (;
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *a = haystack;
+		char *p = needle;
+
+		while (*a == *p && *p != '\0')
+		{
+			a++;
+			p++;
+		}
+		if (*p == '\0')
+			return (haystack);
+	}
+	return (0);
+}
