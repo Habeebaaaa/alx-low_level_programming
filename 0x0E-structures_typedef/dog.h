a2c36896b1dc2743c08db22ae75305e1b8670150
habeebaaaa <habebaghanem9999@gmail.com>
2023-05-23 15:25:48 -0700

    a

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,6 +18,10 @@ struct dog
 };
 typedef struct dog dog_t;
 
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+void free_dog(dog_t *d);
+dog_t *new_dog(char *name, float age, char *owner);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
